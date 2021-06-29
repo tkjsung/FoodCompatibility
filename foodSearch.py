70f6f2f170384f06bde59aa56fa32c55422d4fd3
tkjsung <9490371+tkjsung@users.noreply.github.com>
2021-06-29 11:37:13 -0700

    MVP for foodSearch.py completed.

--- foodSearch.py
+++ foodSearch.py
@@ -46,38 +46,76 @@ def get_lang():
 
 # 3. Actual Search
 def search(data):
-    # Find the index of the search term. Note that this function is written due to repetitiveness.
-    def find_index(tempList):
-        for x in tempList:
+    # 3a. Find the index of the search term. Note that this function is written due to repetitiveness.
+    def find_index(temp_list):
+        for x in temp_list:
             if isinstance(x[1], float):
                 continue
             elif foodTerm in x[1]:
-                temp = x[0] + 1
+                temp = x[0]
                 entry_list.append(temp)
-
-    tempList = list(enumerate(data.item1_EN)); find_index(tempList)
-    tempList = list(enumerate(data.item2_EN)); find_index(tempList)
-    tempList = list(enumerate(data.item3plus_EN)); find_index(tempList)
+    if lang == "en":
+        tempList = list(enumerate(data.item1_EN)); find_index(tempList)
+        tempList = list(enumerate(data.item2_EN)); find_index(tempList)
+        tempList = list(enumerate(data.item3plus_EN)); find_index(tempList)
+    else:
+        tempList = list(enumerate(data.item1)); find_index(tempList)
+        tempList = list(enumerate(data.item2)); find_index(tempList)
+        tempList = list(enumerate(data.item3plus)); find_index(tempList)
 
 
 # 4. Check for duplicates in the entry_list list
-# TODO: remove entries if duplicates are found. Note that the duplicates will both show up as positive
-#       in other words, if "1" is repeated, then both "1"s will be marked as repeated. Figure out a way to circumvent
-#       this issue.
 def check_duplicate():
-    for x in entry_list:
-        if entry_list.count(x) > 1:
-            print("Repeated entry")
-        else:
-            print("Not repeated.")
+    # for x in entry_list:
+    #     if entry_list.count(x) > 1:  # If the same element is detected more than once
+    #         print("Repeated entry")
+    #     else:
+    #         print("Not repeated.")
+    global entry_list
+    entry_list = list(set(entry_list))
+    entry_list.sort()
+
+
+# 5. Display results
+# Could be changed in the future if item3plus contains more than 3 items
+def display_results(data):
+    if len(entry_list) == 0:
+        print("No search results for " + foodTerm + ".")
+    else:
+        print("Search Results for " + foodTerm + ":")
+        for num in entry_list:
+            if lang == "en":
+                food1 = data.item1_EN[num]
+                food2 = data.item2_EN[num]
+                food3 = data.item3plus_EN[num]
+            else:
+                food1 = data.item1[num]
+                food2 = data.item2[num]
+                food3 = data.item3plus[num]
+            compat = str(data.compatibility[num])
+            compat = compat.capitalize()
+            if isinstance(food3, float):
+                del food3
+                print("[" + compat + "] Entry #" + str(num + 1) + ": " + food1 + " & " + food2)
+            else:
+                print("[" + compat + "] Entry #" + str(num + 1) + ": " + food1 + ", " + food2 + " & " + food3)
 
 
 if __name__ == '__main__':
-    df = read_db()
-    # out = get_input()
-    lang = "en"; foodTerm = "broccoli"
-    search(df)
-    check_duplicate()
-    print(entry_list)
-    print("Test123")
+    df = read_db()  # Read database file
+    # out = get_input()  # Get search term from user
+
+    # lang = "en"; foodTerm = "shrimp"
+    lang = "zh"; foodTerm = "蝦"
+
+    search(df)  # Search the term given by user
+
+    # entry_list = [10, 43, 1, 5, 2, 3, 1, 5, 6, 10, 9, 1]
+    # print("orig list: " + str(entry_list))
+
+    check_duplicate()  # Check if the search results contain any duplicates
+    display_results(df)  # Display search results
+
+    # print("new list: " + str(entry_list))
+    print("#### EOP ####")
 
---
+++ gui.py
@@ -0,0 +1,10 @@
+# gui.py
+# Type: Script
+# Author: Tom Sung
+# Purpose: This python script tests Python GUI
+
+# Import relevant libraries
+import tkinter as tk
+
+window = tk.Tk()
+
